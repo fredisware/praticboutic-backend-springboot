c0b276fd99e2c4046db51589a232cc91039b8fc7
Legrand Frédéric <flegrand.info@gmail.com>
2025-04-03 21:28:14 +0200

    Initial commit

--- src/main/java/com/ecommerce/praticboutic_backend_java/PraticbouticBackendJavaApplication.java
+++ src/main/java/com/ecommerce/praticboutic_backend_java/PraticbouticBackendJavaApplication.java
@@ -17,7 +17,7 @@ public class PraticbouticBackendJavaApplication {
 	
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+      return String.format("Hello %s! avec git", name);
     }
 
 }
