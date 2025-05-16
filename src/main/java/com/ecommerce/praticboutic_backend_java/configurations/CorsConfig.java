d8b929b7297fcd8df1571b46a74ec123cad3b3d9
Legrand Frédéric <flegrand.info@gmail.com>
2025-05-16 21:42:27 +0200

    initialcommit_part056

--- src/main/java/com/ecommerce/praticboutic_backend_java/configurations/CorsConfig.java
+++ src/main/java/com/ecommerce/praticboutic_backend_java/configurations/CorsConfig.java
@@ -1,21 +1,29 @@
 package com.ecommerce.praticboutic_backend_java.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
+
+    private final ClientUrlsProperties clientUrlsProperties;
+
+    @Autowired
+    public CorsConfig(ClientUrlsProperties clientUrlsProperties) {
+        this.clientUrlsProperties = clientUrlsProperties;
+    }
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); // Autorise les cookies, headers d'authentification, etc.
-        config.addAllowedOriginPattern("*"); // Permet toutes les origines dynamiquement (remplace allowedOrigins)
+        config.setAllowedOrigins(clientUrlsProperties.getClientUrls()); // Injection de la propriété ici
         config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Accept", "Accept-Language", "X-Authorization"));
         config.setExposedHeaders(Arrays.asList("Content-Length", "X-JSON"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
