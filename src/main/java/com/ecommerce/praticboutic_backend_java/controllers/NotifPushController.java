4837be7435c0cbb15cd6a5a3a1654d22627bc6f6
Legrand Frédéric <flegrand.info@gmail.com>
2025-11-11 16:14:43 +0100

    bugfix_checkversion_part003

--- src/main/java/com/ecommerce/praticboutic_backend_java/controllers/NotifPushController.java
+++ src/main/java/com/ecommerce/praticboutic_backend_java/controllers/NotifPushController.java
@@ -8,16 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import static java.lang.Integer.parseInt;
-
+@RestController
+@RequestMapping("/api")
 public class NotifPushController {
 
     private NotificationService notificationService;
